b9bee320ab1d905f85acd6524dc9c9903c12aa21
Akhil Kasamsetty <awesomesetty99@gmail.com>
2020-08-30 18:46:20 -0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/compilers/zipcode_screen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.compilers.zipcode_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.deepPurpleAccent,
+            title: Center(
+              child: Text('FooDono'),
+            ),
+        ),
+        body: Center(
+          child: Text('Enter your ZipCode:'),
+        ),
+      ),
+    ),
+  );
+}
