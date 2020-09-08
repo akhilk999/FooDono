b570108e791877aa670ecb98266c259cedebe0ce
Akhil Kasamsetty <awesomesetty99@gmail.com>
2020-09-08 14:53:07 -0500

    Added full map to zipCode screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.deepPurpleAccent,
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(33.0198, -96.6989);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController= controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
             title: Center(
-              child: Text('FooDono'),
+              child: Text('Enter Your ZipCode'),
             ),
-        ),
-        body: Center(
-          child: Text('Enter your ZipCode:'),
-        ),
-      ),
-    ),
-  );
+            backgroundColor: Colors.green[700],
+          ),
+          body: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              )
+          ),
+        )
+    );
+  }
 }
