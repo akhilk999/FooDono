cede25dc60f3d1446540f1cfcbfdd15511a903ae
Akhil Kasamsetty <awesomesetty99@gmail.com>
2020-10-11 00:15:36 -0500

    added features to textbox input

--- lib/zipcode.dart
+++ lib/zipcode.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart';
 import 'location.dart';
 
 class ZipCode extends StatefulWidget {
@@ -15,62 +16,111 @@ class _MyAppState extends State<ZipCode> {
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
+  final _formKey = GlobalKey<FormState>();
+  final textController = TextEditingController();
+  int charLength = 0;
+
+  _onChanged(String value) {
+    setState(() {
+      charLength = value.length;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        //resizeToAvoidBottomPadding: false,
         appBar: AppBar(
-          title: Center(
-            child: Text('Enter Your ZipCode'),
-          ),
+          centerTitle: true,
+          title: Text('Enter Your ZipCode'),
           backgroundColor: Colors.green[700],
         ),
-        body: Column(
-          children: [
-            Container(
-              height: 20,
-            ),
-            Container(
-              height: 325,
-                width: 350,
-                child: GoogleMap(
-                  onMapCreated: _onMapCreated,
-                  initialCameraPosition: CameraPosition(
-                  target: LatLng(33.0198, -96.6989), zoom: 16),
-                  )
-              ),
-            Row(
+        body: SingleChildScrollView(
+          child: Column(
             children: [
               Container(
-                width: 45,
+                height: 20,
               ),
               Container(
-                height: 50,
-                width: 140,
-                child: TextField(
-                  onSubmitted: (String str) {
+                height: 325,
+                  width: 350,
+                  child: GoogleMap(
+                    onMapCreated: _onMapCreated,
+                    initialCameraPosition: CameraPosition(
+                    target: LatLng(33.0198, -96.6989), zoom: 16),
+                    )
+                ),
+              Column(
+                  children: [
+                Container(
+                  height: 30,
+                ),
+                Container(
+                    height: 50,
+                    width: 200,
+                    child: Form(
+                      key: _formKey,
+                      child: Row(
+                        children: [
+                        Container(
+                        width: 200.0,
+                        child:TextFormField(
+                            keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            WhitelistingTextInputFormatter.digitsOnly,
+                            LengthLimitingTextInputFormatter(5),
+                          ],
+                          controller: textController,
+                          autocorrect: true,
+                          decoration: InputDecoration(hintText: 'Enter ZipCode Here'),
+                          onChanged: _onChanged,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your ZipCode';
+                            }
+                            if (charLength < 5) {
+                              return 'Please enter all 5 digits';
+                            }
+                            return null;
+                          },
+
+                        ),
+                      ),
+                        ],
+                      ),
+                    )
+                ),
+                    Container(
+                      height: 30,
+                    ),
+                Container(
+                height: 75.0,
+                width: 200.0,
+                padding: EdgeInsets.fromLTRB(50,20,50,20),
+                child: RaisedButton(
+                  child: Text("Submit"),
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      Navigator.pop(context);
+
+                      /*Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Location())
+                    );*/
+                    }
                   },
                 ),
               ),
-              Container(
-              height: 75.0,
-              width: 200.0,
-              padding: EdgeInsets.fromLTRB(50,20,50,20),
-              child: RaisedButton(
-                child: Text("Submit"),
-                onPressed: () {
-                  Navigator.pop(context);
-                  /*Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Location())
-                  );*/
-                },
+              ]
               ),
-            )
             ]
-            )
-          ]
-          ),
+            ),
+        ),
       );
   }
+}
+
+textField() {
+
 }
 
